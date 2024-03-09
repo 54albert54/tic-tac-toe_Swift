312fde63e65e1398da4791d0ab4e202aadbdc4bc
s <as>
2024-03-08 23:40:20 -0500

    Initial Commit

---
+++ ticktock/ticktockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ticktockApp.swift
+//  ticktock
+//
+//  Created by Angel alberto Bernechea on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ticktockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
